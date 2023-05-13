5b4842a9827dbea08a2522165cb0518d1348ced7
Marat Vakhitov <404@bk.ru>
2023-05-13 23:24:47 +0300

    gateway work corret with swagger

---
+++ api-gateway/src/main/java/com/vakhitov/apigateway/configuration/GatewayConfig.java
@@ -0,0 +1,31 @@
+package com.vakhitov.apigateway.configuration;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    @Lazy(false)
+    public List<GroupedOpenApi> apis(RouteDefinitionLocator locator) {
+        List<GroupedOpenApi> groups = new ArrayList<>();
+        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+        for (RouteDefinition definition : definitions) {
+            System.out.println("id: " + definition.getId() + "  " + definition.getUri().toString());
+        }
+        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service")).forEach(routeDefinition -> {
+            String name = routeDefinition.getId().replaceAll("-service", "");
+            GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name).build();
+        });
+        return groups;
+
+    }
+}
